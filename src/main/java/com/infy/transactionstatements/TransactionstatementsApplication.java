9c4ba8b31cf9e7a67d2251781f19efc87b084f6b
shailesh shinde <shai1esh.shinde@yahoo.com>
2021-01-06 13:12:40 +1100

    initial commit

---
+++ src/main/java/com/infy/transactionstatements/TransactionstatementsApplication.java
@@ -0,0 +1,13 @@
+package com.infy.transactionstatements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionstatementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionstatementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infy/transactionstatements/TransactionstatementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.transactionstatements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionstatementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
